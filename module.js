e0a06e527e2425dca2c7dfed8955feb565b1713e
daedalus11069 <daedalus969@gmail.com>
2025-04-22 20:04:35 -0700

    Re-add elevation ruler settings for difficult terrain

--- module.js
+++ module.js
@@ -50,34 +50,10 @@ Hooks.once("setup", () => {
     CONFIG.elevationruler.MOVEMENT_TYPES.TELEPORT
   ] = "portal-enter";
 
-  // CONFIG.elevationruler.pathfindingCheckTerrains = true;
+  CONFIG.elevationruler.pathfindingCheckTerrains = true;
 
-  // CONFIG.elevationruler.SPEED.useFontAwesome = true;
-  // CONFIG.elevationruler.SPEED.terrainSymbol = "\uf071";
-
-  // /**
-  //  * Given a token, get the maximum distance the token can travel for a given type.
-  //  * Distance measured from 0, so types overlap. E.g.
-  //  *   WALK (x1): Token speed 25, distance = 25.
-  //  *   DASH (x2): Token speed 25, distance = 50.
-  //  *
-  //  * @param {Token} token                   Token whose speed should be used
-  //  * @param {SpeedCategory} speedCategory   Category for which the maximum distance is desired
-  //  * @param {number} [tokenSpeed]           Optional token speed to avoid repeated lookups
-  //  * @returns {number}
-  //  */
-  // CONFIG.elevationruler.SPEED.maximumCategoryDistance = function (
-  //   token,
-  //   speedCategory,
-  //   tokenSpeed
-  // ) {
-  //   tokenSpeed ??= CONFIG.elevationruler.SPEED.tokenSpeed(token);
-  //   if (token.actor.system.props.hasParkour) {
-  //     return speedCategory.multiplier * tokenSpeed;
-  //   } else {
-  //     return speedCategory.multiplier * tokenSpeed;
-  //   }
-  // };
+  CONFIG.elevationruler.SPEED.useFontAwesome = true;
+  CONFIG.elevationruler.SPEED.terrainSymbol = "\uf071";
 
   /**
    * Given a token, retrieve its base speed.
